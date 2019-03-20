0a2cdd40244aaeff23965932d04132343f948c1a
Wesss <wesleymcox@gmail.com>
2019-03-20 13:05:55 -0700

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 
-class App extends Component{
+class App extends Component {
     render(){
         return(
-            <div className="App">
-                <h1> Hello, World! </h1>
+            <div class="App">
+                <h1 class="Headline Headline--1"> Hello, World! </h1>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-import * as hello2 from "./hello2.js";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
 import css from './styles.css';
 
-hello2.hello();
-console.log("HELLO SCRIPT!!");
 ReactDOM.render(<App />, document.getElementById("root"));
