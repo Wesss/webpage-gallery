913f7613cd317eb6a53555662d56a4d04009bd2c
Wesss <wesleymcox@gmail.com>
2019-03-22 21:50:35 -0700

    button change of state proof of concept

--- src/app/App.js
+++ src/app/App.js
@@ -2,19 +2,44 @@ import React, { Component } from "react";
 import Headline from "./Headline.js";
 import Clock from "./Clock.js";
 
+const NAV_STATE = {
+    LANDING: "landing",
+    BAR: "bar"
+};
+
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {navState: NAV_STATE.LANDING};
+    }
+
+    clickViewGallery = () => {
+        this.setState({navState: NAV_STATE.BAR});
+    }
+
     render() {
-        return(
-            <div>
-                <div class="Landing">
-                    <div class="Landing-focus">
-                        <Headline headline="Wesley Cox" level={2} inverted={true}/>
-                        <Headline headline="Software Developer" level={1} inverted={true}/>
-                        <button class="Button Button--inverted">View Gallery</button>
+        switch (this.state.navState) {
+            case NAV_STATE.LANDING:
+                return(
+                    <div class="Landing">
+                        <div class="Landing-focus">
+                            <Headline headline="Wesley Cox" level={2} inverted={true}/>
+                            <Headline headline="Software Developer" level={1} inverted={true}/>
+                            <button class="Button Button--inverted" onClick={this.clickViewGallery}>
+                                View Gallery
+                            </button>
+                        </div>
                     </div>
-                </div>
-            </div>
-        );
+                );
+                break;
+            case NAV_STATE.BAR:
+                return(
+                    <div class="Landing">
+                        Bar State
+                    </div>
+                );
+                break;
+        }
 
         /*
         <Clock/>
