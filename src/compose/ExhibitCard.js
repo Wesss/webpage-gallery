d6c58f4e79b24384bc48a96c200562591fab2fd2
Wesss <wesleymcox@gmail.com>
2019-04-06 14:49:42 -0700

    svg draft

--- src/compose/ExhibitCard.js
+++ src/compose/ExhibitCard.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
+// import ExpandButton from 'icon/expand-button.svg';
 import './ExhibitCard.css';
+// <ExpandButton/>
+// <img src="expand-button.svg" alt="expand button"/>
 
 class ExhibitCard extends Component {
   render() {
--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,20 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "babel-loader"
+          },
+          {
+            loader: "react-svg-loader",
+            options: {
+              jsx: true
+            }
+          }
+        ]
       }
     ]
   },
