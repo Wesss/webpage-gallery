f7dad23315a60b8bddf0dd6e2aa45ee2bda06fbf
Wesss <wesleymcox@gmail.com>
2019-04-30 16:00:42 -0700

    split each npm vendor into its own bundle

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   // compilation
   entry: './src/index.js',
   output: {
-    filename: 'index.[hash].js',
+    filename: 'index.[contenthash].js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
@@ -43,6 +43,24 @@ module.exports = {
       filename: 'index.html'
     })
   ],
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+      },
+    },
+  },
   // local dev
   devtool: 'inline-source-map',
   devServer: {
