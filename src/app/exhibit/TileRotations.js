dcd44105be2d73b93079e1358453d199791b7319
Wesss <wesleymcox@gmail.com>
2019-05-21 13:06:16 -0700

    dumb canvas animation

--- src/app/exhibit/TileRotations.js
+++ src/app/exhibit/TileRotations.js
@@ -3,20 +3,46 @@ import "./TileRotations.css";
 
 class TileRotations extends Component {
 
+  constructor(props) {
+    super(props);
+    this.canvasRef = React.createRef();
+    this.state = {frame: 0};
+  }
+
   componentDidMount() {
-    const canvas = this.refs.canvas;
-    const ctx = canvas.getContext("2d");
+    this.canvas = this.canvasRef.current;
+    this.ctx = this.canvas.getContext("2d");
+
+    this.interval = setInterval(
+      () => this.tick(),
+      1000 / 60
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  tick() {
+    var canvas = this.canvas;
+    var ctx = this.ctx;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     ctx.beginPath();
     ctx.moveTo(0, 0);
-    ctx.lineTo(500, 500);
+    ctx.lineTo(500, this.state.frame);
     ctx.stroke();
+
+    this.setState((state) => {
+      state.frame = state.frame + 1;
+      return state;
+    });
   }
 
   render() {
     return(
       // TODO center this in exhibit container
-      <canvas ref="canvas" width={500} height={500}>
+      <canvas ref={this.canvasRef} width={500} height={500}>
       </canvas>
     );
   }
